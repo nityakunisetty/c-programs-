43b0e5d4f1eacb234cde2ff0382cee8e1d7ecd7c
nityakunisetty <kunisettynitya46@gmail.com>
2025-12-10 11:13:11 +0530

    Add files via upload

---
+++ week 1/even no.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=2;i<=n;i+=2) printf("%d ",i);
+}
---
+++ week 1/sum.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int n,s=0;
+    scanf("%d",&n);
+    for(int i=2;i<=n;i+=2) s+=i;
+    printf("%d",s);
+}
